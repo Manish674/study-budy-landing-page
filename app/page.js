4fcd5322acad8813aa2a92e98f484f1dabe152ea
manish674 <manishchoudhary4905@gmail.com>
2023-05-27 22:39:48 +0530

    complete mobile design

--- app/page.js
+++ app/page.js
@@ -1,29 +1,54 @@
 import Image from "next/image";
 
-const FeatureCard = () => {
+const FeatureCard = ({ heading, para }) => {
   return (
-    <div className="">
-      <div className="flex flex-nowrap transform translatex-x-[-100%]">
+    <div className="min-w-[100%]">
+      <div className="flex justify-center">
         <Image
           src="/images/VC.jpg"
           className="rounded-full"
           width={300}
           height={300}
         />
-        <Image src="/images/todo.jpg" width={300} height={300} />
-        <Image src="/images/pomodoro-timer.jpg" width={300} height={300} />
       </div>
       <div className="text-center mt-2">
-        <h3 className="text-2xl font-normal">Virtual Study Hangouts</h3>
-        <p className="text-sm">
-          Connect, collaborate, and excel with seamless video calls. Study
-          together, anytime, anywhere
-        </p>
+        <h3 className="text-2xl font-normal">{heading}</h3>
+        {para}
       </div>
     </div>
   );
 };
 export default function Home() {
+  const featureData = [
+    {
+      heading: "StudyChat: Virtual Study Hangouts",
+      para: (
+        <p className="text-sm">
+          Connect, collaborate, and excel with seamless video calls.{" "}
+          <bold>Study together</bold>, anytime, anywhere
+        </p>
+      ),
+    },
+    {
+      heading: "TaskTrack: Organize and Conquer",
+      para: (
+        <p className="text-sm">
+          Your ultimate sidekick for student success. Organize, conquer, and
+          stay on top of your academic tasks effortlessly
+        </p>
+      ),
+    },
+    // {
+    //   heading: "StudyFlow: The Mastery Tool",
+    //   para: (
+    //     <p>
+    //       Boost your focus and productivity with StudyFlow. Master time
+    //       management and achieve study success effortlessly
+    //     </p>
+    //   ),
+    // },
+  ];
+
   return (
     <div>
       <div className="">
@@ -33,7 +58,7 @@ export default function Home() {
               Connect, Collaborate, and Excel: StudyBuddy <br /> The Ultimate
               Virtual Study Platform
             </h1>
-            <p className="p-4 text-[rgb(95,99,104)] text-center">
+            <p className="p-4 text-[#635f68] text-center">
               Welcome to StudyHub, virtual study platform designed to enhance
               your academic journey. Connect with fellow students through
               immersive video calls, collaborate seamlessly on projects, and
@@ -46,8 +71,61 @@ export default function Home() {
           </button>
         </div>
         <hr className="my-8 w-3/4 mx-auto" />
-        <div className="w-[80%] mx-auto flex overflow-hidden">
-          <FeatureCard />
+        <div className="flex w-[90%] mx-auto">
+          <div className=" my-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10.605"
+              height="15.555"
+              fill="#635f68"
+            >
+              <path d="M10.605 12.727 5.656 7.776l4.949-4.948L7.777 0 0 7.776l7.777 7.779 2.828-2.828z" />
+            </svg>
+          </div>
+
+          <div className="flex flex-nowrap overflow-hidden">
+            {featureData.map((el, i) => (
+              <FeatureCard key={i} heading={el.heading} para={el.para} />
+            ))}
+          </div>
+
+          <div className=" my-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10.605"
+              height="15.555"
+              fill="#635f68"
+            >
+              <path d="m2.828 15.555 7.777-7.779L2.828 0 0 2.828l4.949 4.948L0 12.727l2.828 2.828z" />
+            </svg>
+          </div>
+        </div>
+        <div className="mt-8">
+          <div>
+            <h2 className="font-semibold text-center text-4xl">
+              Join the waitlist
+            </h2>
+            <form className="mt-8 flex flex-col space-y-4 w-[80%] mx-auto">
+              <input
+                name="name"
+                placeholder="name"
+                className="p-2 border border-[#ebebed] outline-none"
+              />
+              <input
+                name="email"
+                placeholder="email"
+                className="p-2 border border-[#ebebed] outline-none"
+              />
+              <textarea
+                name="thoughts"
+                placeholder="Thoughts? Share below (optional)"
+                className="p-2 border border-[#ebebed] outline-none h-[152px]"
+              />
+              <button className="bg-[#e92761] text-white p-4 rounded-lg  ">
+                Join waitlist
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
