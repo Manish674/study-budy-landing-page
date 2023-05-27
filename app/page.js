752b06d56d448741ad55017dffa937d416068683
manish674 <manishchoudhary4905@gmail.com>
2023-05-27 23:44:12 +0530

    added pc designs

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const FeatureCard = ({ heading, para }) => {
   return (
@@ -51,67 +52,76 @@ export default function Home() {
 
   return (
     <div>
-      <div className="">
-        <div className="mt-16 flex flex-col items-center justify-center">
-          <div className="">
-            <h1 className="font-semibold text-center text-4xl">
-              Connect, Collaborate, and Excel: StudyBuddy <br /> The Ultimate
-              Virtual Study Platform
-            </h1>
-            <p className="p-4 text-[#635f68] text-center">
-              Welcome to StudyHub, virtual study platform designed to enhance
-              your academic journey. Connect with fellow students through
-              immersive video calls, collaborate seamlessly on projects, and
-              unlock your full potential with our suite of productivity
-              features.{" "}
-            </p>
+      <div>
+        <div className="lg:flex lg:h-screen items-center justify-around">
+          <div className=" w-[50%] mt-16 flex flex-col items-center justify-center">
+            <div className="">
+              <h1 className="font-semibold text-center text-3xl">
+                StudyBuddy: Connect, Collaborate, Excel <br /> The Ultimate
+                Virtual Study Platform
+              </h1>
+              <p className="p-4 text-[#635f68] text-center">
+                Welcome to StudyHub, virtual study platform designed to enhance
+                your academic journey. Connect with fellow students through
+                immersive video calls, collaborate seamlessly on projects, and
+                unlock your full potential with our suite of productivity
+                features.{" "}
+              </p>
+            </div>
+            <Link href={'#join-waitlist'}>
+              <button className="mt-4 bg-[#1a73e8] p-4 rounded-lg text-white font-medium">
+                Join waitlist
+              </button>
+            </Link>
           </div>
-          <button className="mt-4 bg-[#1a73e8] p-4 rounded-lg text-white font-medium">
-            Join waitlist
-          </button>
-        </div>
-        <hr className="my-8 w-3/4 mx-auto" />
-        <div className="flex w-[90%] mx-auto">
-          <div className=" my-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="10.605"
-              height="15.555"
-              fill="#635f68"
-            >
-              <path d="M10.605 12.727 5.656 7.776l4.949-4.948L7.777 0 0 7.776l7.777 7.779 2.828-2.828z" />
-            </svg>
-          </div>
+          <hr className="my-8 w-3/4 mx-auto lg:hidden" />
+          <div className="lg:w-[40%] flex w-[90%] mx-auto lg:mx-0">
+            <div className=" my-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10.605"
+                height="15.555"
+                fill="#635f68"
+              >
+                <path d="M10.605 12.727 5.656 7.776l4.949-4.948L7.777 0 0 7.776l7.777 7.779 2.828-2.828z" />
+              </svg>
+            </div>
 
-          <div className="flex flex-nowrap overflow-hidden">
-            {featureData.map((el, i) => (
-              <FeatureCard key={i} heading={el.heading} para={el.para} />
-            ))}
-          </div>
+            <div className="flex flex-nowrap overflow-hidden">
+              {featureData.map((el, i) => (
+                <FeatureCard key={i} heading={el.heading} para={el.para} />
+              ))}
+            </div>
 
-          <div className=" my-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="10.605"
-              height="15.555"
-              fill="#635f68"
-            >
-              <path d="m2.828 15.555 7.777-7.779L2.828 0 0 2.828l4.949 4.948L0 12.727l2.828 2.828z" />
-            </svg>
+            <div className=" my-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10.605"
+                height="15.555"
+                fill="#635f68"
+              >
+                <path d="m2.828 15.555 7.777-7.779L2.828 0 0 2.828l4.949 4.948L0 12.727l2.828 2.828z" />
+              </svg>
+            </div>
           </div>
         </div>
-        <div className="mt-8">
-          <div>
+        <div
+          id="join-waitlist"
+          className="max-w-[600px] mx-auto lg:h-screen lg:flex items-center"
+        >
+          <div className="lg:w-full">
             <h2 className="font-semibold text-center text-4xl">
               Join the waitlist
             </h2>
             <form className="mt-8 flex flex-col space-y-4 w-[80%] mx-auto">
               <input
+                autoComplete="off"
                 name="name"
                 placeholder="name"
                 className="p-2 border border-[#ebebed] outline-none"
               />
               <input
+                autoComplete="off"
                 name="email"
                 placeholder="email"
                 className="p-2 border border-[#ebebed] outline-none"
